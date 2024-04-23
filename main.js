60726f5c32aa7b772e5b1380fc5d2ef412cd4df4
Jeffrey Doyle <doylejeff98@gmail.com>
2024-04-23 14:25:39 -0400

    scroll reveal for gallery and video headers

--- main.js
+++ main.js
@@ -7,6 +7,45 @@ const scrollRevealOption = {
   ScrollReveal().reveal(".section__container h3", {
     ...scrollRevealOption,
   });
+  ScrollReveal().reveal(".grid", {
+    ...scrollRevealOption,
+    origin: "top",
+    delay: 300,
+  });
+  ScrollReveal().reveal(".grid__2", {
+    ...scrollRevealOption,
+    origin: "left",
+    delay: 500,
+  });
+  ScrollReveal().reveal(".grid__3", {
+    ...scrollRevealOption,
+    origin: "right",
+    delay: 600,
+  });
+  ScrollReveal().reveal(".gallery", {
+    ...scrollRevealOption,
+    origin: "top",
+    delay: 300,
+  });
+  ScrollReveal().reveal(".beauty__video", {
+    ...scrollRevealOption,
+    origin: "left",
+    delay: 100,
+  });
+  ScrollReveal().reveal(".section__title--subtitle", {
+    ...scrollRevealOption,
+    origin: "right",
+  });
+  ScrollReveal().reveal(".section__title--sub", {
+    ...scrollRevealOption,
+    origin: "top",
+    delay: 600,
+  });
+  ScrollReveal().reveal(".third__header", {
+    ...scrollRevealOption,
+    origin: "right",
+    delay: 400,
+  });
   ScrollReveal().reveal(".section__container h1", {
     ...scrollRevealOption,
     origin: "left",
